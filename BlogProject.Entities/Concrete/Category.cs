69f7564fc08cb8eaa200530908b43497a29fd043
yunus şen <yunussen2727@gmail.com>
2021-02-01 18:36:44 +0300

    Add project files.

---
+++ BlogProject.Entities/Concrete/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogProject.Entities.Abstract;
+namespace BlogProject.Entities.Concrete
+{
+
+
+    public class Category : EntityBase, IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Article> Articles { get; set; }
+    }
+}
