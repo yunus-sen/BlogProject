f971dbd25c94372f8b8561ed3d9ee19b0cf54f9d
yunus şen <yunussen2727@gmail.com>
2021-02-03 19:17:23 +0300

    Result class yazıldı.

---
+++ BlogProject.Shared/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,16 @@
+﻿using BlogProject.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Shared.Utilities.Results.Abstract
+{
+    public interface IResult
+    {
+        public ResultStatus ResultStatus { get;}
+        public string Message { get;}
+        public Exception Exception { get;}
+    }
+}
---
+++ BlogProject.Shared/Utilities/Results/Concrete/Result.cs
@@ -0,0 +1,35 @@
+﻿using BlogProject.Shared.Utilities.Results.Abstract;
+using BlogProject.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Shared.Utilities.Results.Concrete
+{
+    public class Result : IResult
+    {
+        public Result(ResultStatus ResultStatus)
+        {
+            this.ResultStatus = ResultStatus;
+        }
+        public Result(ResultStatus ResultStatus, string Message)
+        {
+            this.ResultStatus = ResultStatus;
+            this.Message = Message;
+        }
+
+        public Result(ResultStatus ResultStatus, string Message, Exception Exception)
+        {
+            this.ResultStatus = ResultStatus;
+            this.Message = Message;
+            this.Exception = Exception;
+        }
+        public ResultStatus ResultStatus { get; }
+
+        public string Message { get; }
+
+        public Exception Exception { get; }
+    }
+}
